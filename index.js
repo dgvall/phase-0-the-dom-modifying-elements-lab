87c34110cb642ba34cb84b4c16f25909a9ccdc5b
Diego Valles <dgvall134@gmail.com>
2022-09-13 15:23:38 -0600

    commit tree 16f9fc0bee3481d066cd57882e547516f799bf16

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+// Functional removing of element in the DOM
+const main = document.getElementById("main")
+main.remove()
+// Functional creation of element in the DOM
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+// Giving value to the element in the DOM
+newHeader.textContent = "Diego is the champion"
